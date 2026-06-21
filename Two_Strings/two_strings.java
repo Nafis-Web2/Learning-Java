44eace4bfea1a8736c0539ebb3c2d802a0e63852
Nafis-Web2 <NAFIS.HOSSAIN22@student.qcc.cuny.edu>
2026-06-21 19:55:34 -0400

    Java- Variable excerise

---
+++ Two_Strings/two_strings.java
@@ -0,0 +1,14 @@
+package Two_Strings;
+
+public class two_strings {
+    public static void main(String[] args) {
+    String str1 = "Hello";
+    String str2 = "World";
+
+    String result = str1 + " " + str2;
+
+    System.out.println(result);
+    System.out.println(" ");
+    
+}
+}
